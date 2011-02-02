4f2e37216b85e91505a01e8cec08c61b0b26f77a
Frederick Ostrander <fred@sullenfish.com>
2011-02-02 08:48:15 -0600

    Added actual code and README text.
    Signed-off-by: Frederick Ostrander <fred@sullenfish.com>

---
+++ README.md
@@ -0,0 +1,7 @@
+# mooLookupTable
+## for MooTools 1.3
+MooLookupTable is a MooTools Class for transforming generic table elements into lookup tables, all in client-side JavaScript.
+
+It relies havily on Slick, so MooTools Core 1.3 is a prerequisite.
+
+Copyright &copy; 2011 Frederick J. Ostrander
---
+++ mooLookupTable.js
@@ -0,0 +1,42 @@
+var MooLookupTable = new Class({
+    initialize: function(table, primaryKey) {
+        if (table) {
+            this.table = table;
+        } else {
+            return false;
+        }
+        // set primary key
+        switch (typeOf(primaryKey)) {
+        case 'element':
+        case 'textnode':
+            primaryKey = primaryKey.get('text');
+            break;
+        case 'string':
+            break;
+        default:
+            var pk = this.table.getElement('thead .primary-key');
+            primaryKey = pk ? pk.get('text') : null;
+        }
+        this.primaryKey = primaryKey ? primaryKey : this.table.getElement('th').get('text');
+    },
+    lookup: function(primaryKeyValue, keys) {
+        if (!primaryKeyValue || !keys) {
+            return null;
+        }
+        keys = Array.from(keys);
+        var values = [],
+            primaryColumn = this.table.getElements('thead ^ tr th').map(function(item) {
+                return item.get('text');
+            }).indexOf(this.primaryKey),
+            primaryRow = this.table.getElements('tbody tr td:nth-child(' + (primaryColumn + 1) + ')').map(function(item) {
+                return item.get('text');
+            }).indexOf(primaryKeyValue);
+        keys.each(function(key) {
+            var keyColumn = this.table.getElements('thead ^ tr th').map(function(item) {
+                return item.get('text');
+            }).indexOf(key);
+            values.push(this.table.getElement('tbody tr:nth-child(' + (primaryRow + 1) + ') td:nth-child(' + (keyColumn + 1) + ')').get('text'));
+        }, this);
+        return values;
+    }
+});
